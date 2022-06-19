6ab7e65dfdbb2dbb9b1d9530b18204d4fba147fb
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2022-06-19 22:44:16 +0300

    some tasks added

---
+++ Viktoria_Shevliuk/task2/src/com/SoftServe/edu/task2/Main.java
@@ -0,0 +1,13 @@
+package com.SoftServe.edu.task2;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+	    System.out.println("How are you? ");
+	    String ans = sc.nextLine();
+	    System.out.println("You are " + ans);
+    }
+}
