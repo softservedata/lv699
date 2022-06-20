6f7903fcf53d4f49eafef54564fda36fca873d81
Jaroslav <garasym@gmail.com>
2022-06-20 19:14:19 +0300

    Appl01 Updated

--- src/com/softserve/hw01/App01.java
+++ src/com/softserve/hw01/App01.java
@@ -3,6 +3,6 @@ package com.softserve.hw01;
 public class App01 {
 
     public static void main(String[] args) {
-        System.out.printf("Task01");
+        System.out.printf("Task01 Example");
     }
 }
