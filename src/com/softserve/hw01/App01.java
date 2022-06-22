51eb942422445fea251b88e43279740a50360c57
bohdanviter <bohdanviter@gmail.com>
2022-06-22 17:23:28 +0300

    Add Hello from Eclipse

--- src/com/softserve/hw01/App01.java
+++ src/com/softserve/hw01/App01.java
@@ -3,5 +3,6 @@ package com.softserve.hw01;
 public class App01 {
     public static void main(String[] args) {
         System.out.printf("Task01 Example");
+        System.out.println("Hello from Eclipse");
     }
 }
