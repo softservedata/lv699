ce440142d0110cfed357f0743e345024b9dcc833
Volodymyr <finansist33333324@gmail.com>
2022-06-22 12:09:01 +0300

    add new files

--- src/com/softserve/hw02/App2.java
+++ src/com/softserve/hw02/App2.java
@@ -1,7 +1,7 @@
 package com.softserve.hw02;
 
 public class App2 {
-    public static void main (String args[]){
-        System.out.println("Hello");
+    public static void main (String[] args){
+        System.out.println("Hello ");
     }
 }
