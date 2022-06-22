958343d279b10f7ecac16209360bac3bb6005e01
Volodymyr <finansist33333324@gmail.com>
2022-06-22 11:23:35 +0300

    add new files

--- src/com/softserve/hw01/App01.java
+++ src/com/softserve/hw01/App01.java
@@ -3,6 +3,8 @@ package com.softserve.hw01;
 public class App01 {
 
     public static void main(String[] args) {
-        System.out.printf("Task01");
+
+        System.out.println("Task01 Example");
+        System.out.println("Hell");
     }
 }
---
+++ src/com/softserve/hw02/App2.java
@@ -0,0 +1,7 @@
+package com.softserve.hw02;
+
+public class App2 {
+    public static void main (String args[]){
+        System.out.println("Hello");
+    }
+}
