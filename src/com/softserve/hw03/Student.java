baf0e77de5527f3bda71713f6da164ff34f66977
Liuba_Vivcharchyn <l.wiwczarczyn@gmail.com>
2022-06-30 16:20:47 +0200

    practical  part1

--- src/com/softserve/hw03/Student.java
+++ src/com/softserve/hw03/Student.java
@@ -2,9 +2,9 @@ package com.softserve.hw03;
 
 public class Student {
     private String name;
-boolean result;
     private int rating;
     public static double avgRating = 0;
+
 
                 public static double getAvgRating() {
                     return avgRating;
@@ -24,49 +24,61 @@ boolean result;
 
                     public void setRating(int rating) {
                         this.rating = rating;
+                        avgRating = avgRating + rating;
                     }
 
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", rating=" + rating +
+                '}';
+    }
 
     public Student () {
         name="";
-        rating=-1;
-        avgRating=avgRating + rating;}
+        rating=0;
+        }
 
     public Student (String name, int rating) {
         this.name=name;
         this.rating=rating;
-        avgRating=avgRating + rating;
+        avgRating = avgRating + rating;
+    }
+            public boolean betterStudent(Student a, Student b){
+                    return a.rating>b.rating;
+            }
+    public static void main (String[] args) {
+        // create 3 objects and input information about them
+        Student student1 = new Student();
+        student1.setName("Dave");
+        student1.setRating(81);
+        Student student2 = new Student("maria",70);
+        Student student3 = new Student("Diana",99);
+
+        // Display total information about students with rating
+
+        System.out.println(student1.toString());
+        System.out.println(student2.toString());
+        System.out.println(student3.toString());
+
+       //  Displaythe average and total rating of all student
+        System.out.println("Total Rating of all students = " + Student.getAvgRating());
+        System.out.println("st2>st1 = " + student3.betterStudent(student2, student1));
+
+        System.out.format("Average Rating = " + "%.2f", Student.getAvgRating()/3);
+
+
+
+
+
+
+
+
+        }
+
+
     }
 
-  // допрацювати
-   /*public boolean betterStudent(int rating) {
-       result= rating > avgRating;
-                    return true;
-
-         }*/
 
 
-   // betterStudent - to definite the better student (between two, return true or false)
-   public String toString() {
-       return "Student{" +
-               "name:" + name +
-               ", rating = " + rating + '}';
-
-   }
-
-public static void main (String[] args) {
-        Student student1 = new Student("Dave",85);
-    System.out.println(student1.toString());
-   // System.out.println(student1.betterStudent(85));
-        Student student2 = new Student("maria",70);
-    System.out.println(student2.toString());
-   // System.out.println(student2.betterStudent(70));
-        Student student3 = new Student("Diana",99);
-    System.out.println(student3.toString());
-   // System.out.println(student3.betterStudent(99));
-
-        System.out.println("avgRating = " + Student.getAvgRating()/3);
-
-}
-
-}
