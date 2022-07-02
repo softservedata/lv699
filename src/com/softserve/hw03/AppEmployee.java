902d8b550a496958c6e39abd32c58ba7b603d7fe
Volodymyr <finansist33333324@gmail.com>
2022-07-02 13:55:22 +0300

    Practical task

---
+++ src/com/softserve/hw03/AppEmployee.java
@@ -0,0 +1,19 @@
+package com.softserve.hw03;
+
+public class AppEmployee {
+    public static void main(String[] args) {
+        Employee emp1 = new Employee("Ivan", 100,40);
+        System.out.println("emp1.getSalary()= "+ emp1.getSalary());
+        System.out.println("emp1.getSalary()= "+ emp1.getSalary());
+        Employee emp2 = new Employee("Petro", 100,20);
+        System.out.println("emp2.getSalary()= "+ emp2.getSalary());
+        Employee emp3 = new Employee("Vovan", 80,30);
+        System.out.println("emp3.getSalary()= "+ emp3.getSalary());
+        //
+        emp1.changeRate(200);
+        System.out.println("Updated emp1.getSalary()= "+ emp1.getSalary());
+        //
+        System.out.println("getTotalSum = "+ Employee.getTotalSum());
+
+    }
+}
