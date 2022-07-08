ac380ee58fe2180d095038f8907d39699b8bfddb
4Vitalii5 <pavlykvitalii@gmail.com>
2022-07-08 19:09:19 +0300

    homework04 practical hw04 first task

--- src/com/softserve/hw04/home_work/First.java
+++ src/com/softserve/hw04/home_work/First.java
@@ -1,12 +1,15 @@
 package com.softserve.hw04.home_work;
 
-import com.softserve.hw04.home_work.dao.MyIn;
-import com.softserve.hw04.home_work.dao.MyOut;
-import com.softserve.hw04.home_work.serv.BusLogic;
+import java.util.Scanner;
 
 public class First {
 
     public static void main(String[] args) {
-        new BusLogic(new MyIn(), new MyOut()).add();
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your numbers:");
+        Range range = new Range(sc.nextFloat(), sc.nextFloat(),sc.nextFloat());
+        System.out.println("Your numbers: "+range.toString());
+
+        System.out.println("Numbers are belong range [-5,5]:"+ range.belong());
     }
 }
--- src/com/softserve/hw04/home_work/dao/In.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.hw04.home_work.dao;
-
-public interface In {
-
-    float readFloat(float arg0);
-    void close();
-}
