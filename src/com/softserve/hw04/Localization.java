2f6052f8ad46d8501ed013fe233bc7e2d88a95a5
4Vitalii5 <pavlykvitalii@gmail.com>
2022-07-10 20:53:01 +0300

    homework04 practical hw04 adding enum

--- src/com/softserve/hw04/Localization.java
+++ src/com/softserve/hw04/Localization.java
@@ -1,20 +1,21 @@
 package com.softserve.hw04;
 
 public enum Localization {
-EN("англійська", "English"), UA("українська", "Ukrainian");
-private final String ua;
-private final String en;
+    EN("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "English"), UA("пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ", "Ukrainian");
+    private final String ua;
+    private final String en;
 
-Localization(String ua, String en) {
-this.ua = ua;
-this.en = en;
-}
+    Localization(String ua, String en) {
+        this.ua = ua;
+        this.en = en;
+    }
 
-public String getUa() {
-return ua;
-}
+    public String getUa() {
+        return ua;
+    }
 
-public String getEn() {
-return en;
+    public String getEn() {
+        return en;
+    }
 }
 
