abdb32089405be338654665049f1fdf2964cd1b0
4Vitalii5 <pavlykvitalii@gmail.com>
2022-07-10 22:39:31 +0300

    homework04 practical hw04 test for first task

--- src/com/softserve/hw04/Localization.java
+++ src/com/softserve/hw04/Localization.java
@@ -1,20 +1,21 @@
 package com.softserve.hw04;
 
 public enum Localization {
-EN("���������", "English"), UA("���������", "Ukrainian");
-private final String ua;
-private final String en;
+    EN("���������", "English"), UA("���������", "Ukrainian");
+    private final String ua;
+    private final String en;
 
-Localization(String ua, String en) {
-this.ua = ua;
-this.en = en;
-}
+    Localization(String ua, String en) {
+        this.ua = ua;
+        this.en = en;
+    }
 
-public String getUa() {
-return ua;
-}
+    public String getUa() {
+        return ua;
+    }
 
-public String getEn() {
-return en;
+    public String getEn() {
+        return en;
+    }
 }
 
--- src/com/softserve/hw04/home_work/Dogs.java
+++ src/com/softserve/hw04/home_work/Dogs.java
@@ -4,7 +4,7 @@ public class Dogs {
     public static void main(String[] args) {
         Dog dog1= new Dog("Sally", Breed.DOB, 3);
         Dog dog2= new Dog("Mike", Breed.DVOR, 8);
-        Dog dog3= new Dog("Jack", Breed.BULDOG, 10);
+        Dog dog3= new Dog("Jack", Breed.BULDOG, 1);
         System.out.println("First dog:"+"\n"+dog1.toString());
         System.out.println("Second dog:"+"\n"+dog2.toString());
         System.out.println("Third dog:"+"\n"+dog3.toString());
