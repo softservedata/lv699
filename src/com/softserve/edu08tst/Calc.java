3ed1ada7788e84b190aa95b10b012ad0b2193bc2
pasha699 <gavrishko.pavlo@gmail.com>
2022-07-10 03:17:08 +0300

    JUnitTest

---
+++ src/com/softserve/edu08tst/Calc.java
@@ -0,0 +1,13 @@
+package com.softserve.edu08tst;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Calc {
+    public double add(double arg0, double arg1) {
+        return arg0 + arg1;
+    }
+
+    public double div(double arg0, double arg1){return arg0/arg1};
+}
