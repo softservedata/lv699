86b9968f46839567169fde26ec3247b303e73a6b
Liuba_Vivcharchyn <l.wiwczarczyn@gmail.com>
2022-07-17 21:12:28 +0200

    Home work Employee / Developer

--- src/com/softserve/hw06/part2/Developer.java
+++ src/com/softserve/hw06/part2/Developer.java
@@ -3,8 +3,6 @@ package com.softserve.hw06.part2;
 public class Developer extends Employee {
     private String position;
 
-
-
     public Developer(String name, int age, double salary, String position) {
         super(name, age, salary);
         this.position = position;
--- src/com/softserve/hw06/part2/Employee.java
+++ src/com/softserve/hw06/part2/Employee.java
@@ -20,23 +20,14 @@ public class Employee {
         return name;
     }
 
-    public void setName(String name) {
-        this.name = name;
-    }
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-    public double getSalary() {
+       public double getSalary() {
         return salary;
     }
 
-    public void setSalary(double salary) {
-        this.salary = salary;
-    }
+
 }
