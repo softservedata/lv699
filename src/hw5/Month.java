7b47d0517c5f7e65be33dec03c2bc95cca70ab54
stelmaschyk <stelmaschyk.vg@gmail.com>
2022-07-22 23:05:37 +0300

    hw5

--- src/hw5/Month.java
+++ src/hw5/Month.java
@@ -18,12 +18,11 @@ public class Month {
 
 //	2. Enter 10 integer numbers. Calculate the sum of first 5 elements if they are positive or product of last 5 element in the other case.	
 
-		    
 		int[] num = new int[10];
 		for (int i = 0; i < num.length; i++) {
 			num[i] = -1;
 		}
-			int n = 0;
+		int n = 0;
 		for (int i = 0; i < num.length; i++) {
 			System.out.print("Please enter 10 number for add to array ");
 			n = (sc.nextInt());
@@ -33,6 +32,7 @@ public class Month {
 			num[i] = n;
 		}
 		System.out.println("Array num " + Arrays.toString(num));
+		
 // sum of first 5 elements
 		int sumF = 0;
 		for (int i = 0; i < num.length; i++) {
@@ -42,20 +42,21 @@ public class Month {
 			sumF += num[i];
 		}
 		System.out.println("Total summ the first five number = " + sumF);
-// Dobutok of last 5 elements
-		int dobutok = 1;
-		for (int i = 5; i < num.length; i++) {
-			dobutok = dobutok * num[i];
-		}
-		System.out.println("Dobutok the last five number = " + dobutok);
-	
-// Enter 5 integer numbers. Find  position of second positive number;
 		
-		int [] fiveNumber = new int [5];
+// Product of last 5 elements
+		int Product = 1;
+		for (int i = 5; i < num.length; i++) {
+			Product = Product * num[i];
+		}
+		System.out.println("Product the last five number = " + Product);
+
+// Enter 5 integer numbers. Find  position of second positive number;
+
+		int[] fiveNumber = new int[5];
 		for (int i = 0; i < fiveNumber.length; i++) {
 			System.out.print("\nPlease enter 5 number for add to array ");
 			fiveNumber[i] = (sc.nextInt());
-			}
+		}
 		System.out.println("Array fiveNumber " + Arrays.toString(fiveNumber));
 		sc.close();
 		for (int i = 0; i < fiveNumber.length; i++) {
@@ -64,12 +65,32 @@ public class Month {
 			if (fiveNumber[i] > 0) {
 				int positionInArray = i + 1;
 				System.out.println(fiveNumber[i] + " - this number in array on " + positionInArray + " position");
+
+// minimum and its position in the array
+				int min = fiveNumber[0];
+				int imin = 0;
+				while (i < fiveNumber.length) {
+					if (fiveNumber[i] < min) {
+						min = fiveNumber[i];
+						imin = i;
+					}
+					i++;
+				}
+				System.out.println("Minimum number = " + min + ". Position in array " + (imin + 1));
 			}
+
+// Count the product of all entered even numbers
+			int productall = 1;
+			for (int h = 0; h < fiveNumber.length; h++) {
+
+				if (fiveNumber[h] != 0) {
+					if (fiveNumber[h] % 2 == 0) {
+						productall = productall * fiveNumber[h];
+					}
+				}
+			}
+			System.out.println("Product of numbers = " + productall);
 		}
+
 	}
 }
-	
-	
-	
-
-
