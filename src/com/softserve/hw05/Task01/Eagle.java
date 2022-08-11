a31002c741d1bc22c095c1c6485db1c8a0894af5
pasha699 <gavrishko.pavlo@gmail.com>
2022-08-11 13:05:43 +0300

    11.08.22 Homework

---
+++ src/com/softserve/hw05/Task01/Eagle.java
@@ -0,0 +1,21 @@
+package com.softserve.hw05.Task01;
+
+public class Eagle extends FlyingBird{
+
+    public Eagle(String feathers, int layEggs) {
+        super(feathers, layEggs);
+    }
+
+    @Override
+    public void fly(){
+          System.out.println("I am Eagle and i can fly");
+      }
+
+    @Override
+    public String toString() {
+        return "Eagle{" +
+                "feathers='" + getFeathers() + '\'' +
+                ", layEggs=" + getLayEggs() +
+                '}';
+    }
+}
---
+++ src/com/softserve/hw05/Task01/Swallow.java
@@ -0,0 +1,20 @@
+package com.softserve.hw05.Task01;
+
+public class Swallow extends FlyingBird {
+    public Swallow(String feathers, int layEggs) {
+        super(feathers, layEggs);
+    }
+
+    @Override
+    public void fly(){
+        System.out.println("I am Swallow and i can fly");
+    }
+
+    @Override
+    public String toString() {
+        return "Swallow{" +
+                "feathers='" + getFeathers() + '\'' +
+                ", layEggs=" + getLayEggs() +
+                '}';
+    }
+}
