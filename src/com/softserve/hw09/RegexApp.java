254eace03fd87a0a706cb4e38dcfb575e226dcfa
bohdanviter <bohdanviter@gmail.com>
2022-08-25 09:33:53 +0300

    hw10 String, RegEx

--- src/com/softserve/hw09/RegexApp.java
+++
@@ -1,7 +0,0 @@
-package com.softserve.hw09;
-
-public class RegexApp {
-    public static void main(String[] args) {
-        System.out.println("Trying if the git works on new computer");
-    }
-}
