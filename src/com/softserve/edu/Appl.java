a80b6f513855ff6e48bb810da87a95013d39e207
Jaroslav <garasym@gmail.com>
2022-08-31 20:18:33 +0300

    Code Convention

---
+++ src/com/softserve/edu/Appl.java
@@ -0,0 +1,27 @@
+/*
+* Copyright (c) Sun Microsystems, Inc.
+* This software is
+*/
+package com.softserve.edu;
+
+/**
+ * Class <i>description</i> goes here.
+ * @author Lv-699.JavaCore
+ * @version 1.82 18 Mar 2016
+ */
+public final class Appl {
+
+    /**
+     * <font color="red" size=5 >Default</font> <b>Constructor</b>.
+     */
+    private Appl() {
+    }
+
+    /**
+     * @param args from commnd line.
+     */
+    public static void main(final String[] args) {
+        System.out.println("Hello");
+    }
+
+}
---
+++ src/com/softserve/edu/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * <font color="red" size=5 >Super</font> Package.
+ */
+package com.softserve.edu;
