aea38337721d42eda5a7d412f55360f217e7c2fd
Liuba_Vivcharchyn <l.wiwczarczyn@gmail.com>
2022-08-31 23:39:27 +0200

    task 2 HW - 9.Collection

---
+++ src/com/softserve/hw09/homework1/task2/Task2.java
@@ -0,0 +1,43 @@
+package com.softserve.hw09.homework1.task2;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
+
+public class Task2 {
+
+
+
+    public static void main(String[] args) {
+        //Create map personMap and add to it ten persons of type <String, String> (lastName, firstName).
+        Map<String, String> personMap = new HashMap<>();
+        personMap.put("Moroz","Mykyta" );
+        personMap.put("Lys", "Anna");
+        personMap.put("Wild", "Volodymyr");
+        personMap.put("Snow", "Elizabeth");
+        personMap.put("Koval", "Orest");
+        personMap.put("Park", "John");
+        personMap.put("Vstanka", "Valeria");
+        personMap.put("Karpovych", "Petro");
+        personMap.put("Valigora", "Orest");
+        personMap.put("Kovalski", "Damian");
+
+        //Output the entities of the map on the screen.
+        // There are at less two persons with the same firstName among these 10 people
+        System.out.println("Map Entities: " + personMap);
+
+        // Remove from the map person whose firstName is ”Orest” (or other). Print result.
+        Iterator iterator = personMap.entrySet().iterator();
+        while (iterator.hasNext()) {
+            Map.Entry<String, String> entry = (Map.Entry)iterator.next();
+            if (entry.getValue().equals("Orest")){
+                String key = entry.getKey();
+                String value = entry.getValue();
+                System.out.println("Orest " + entry.getKey() + " is going to be removed");
+                iterator.remove();
+            }
+        }
+        System.out.println("Map Entities after removing: " + personMap);
+        }
+
+    }
