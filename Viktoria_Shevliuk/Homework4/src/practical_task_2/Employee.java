317a500bc95f8a67a3a384ca1b4583d999c0e3cf
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2022-09-12 23:26:20 +0300

    started hw4 pt2

---
+++ Viktoria_Shevliuk/Homework4/src/practical_task_2/Employee.java
@@ -0,0 +1,31 @@
+package practical_task_2;
+
+public class Employee {
+    private String name;
+    private int depnum;
+    private double salary;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getDepnum() {
+        return depnum;
+    }
+
+    public void setDepnum(int depnum) {
+        this.depnum = depnum;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+}
---
+++ Viktoria_Shevliuk/Homework4/src/practical_task_2/Run.java
@@ -0,0 +1,7 @@
+package practical_task_2;
+
+public class Run {
+    public static void main(String[] args) {
+
+    }
+}
