8e59f3f3af356b4638930e3c55e495da685610d2
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2022-09-29 20:03:55 +0300

    homework4 hw2 started

---
+++ Viktoria_Shevliuk/Homework4/src/hw2/Run.java
@@ -0,0 +1,37 @@
+/*Enter 10 integer numbers.
+Calculate the sum of first 5 elements if they are positive
+ or product of last 5 element in the other case.
+ */
+package hw2;
+
+public class Run {
+    public static void main(String[] args) {
+        int [] numbers = {3, 0, -7, 4, -1, 10, 65, -3, 35, 1};
+        int sum = 0;
+        int product = 1;
+
+        boolean positive = false;
+        for(int i = 0; i<5; i++){
+
+        }
+
+
+//        for(int num : numbers){
+//            if (num > 0 ){
+//               for(int i=0; i<5; i++){
+//                   sum += num;
+//               }
+//
+//            } else {
+//                for(int i=5; i<10; i++){
+//                    product = product*num;
+//                }
+//
+//            }
+//        }
+//        System.out.println("The sum of the first 5 elements is " + sum );
+//        System.out.println("The product of the last 5 elements is " + product );
+
+    }
+
+}
