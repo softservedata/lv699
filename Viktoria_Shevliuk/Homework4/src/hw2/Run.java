8522dacf8061f231ee5601533d0ee70f1f475bd5
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2022-10-17 17:54:06 +0300

    hw 4.2 updated

--- Viktoria_Shevliuk/Homework4/src/hw2/Run.java
+++ Viktoria_Shevliuk/Homework4/src/hw2/Run.java
@@ -2,36 +2,24 @@
 Calculate the sum of first 5 elements if they are positive
  or product of last 5 element in the other case.
  */
+
 package hw2;
 
 public class Run {
+
     public static void main(String[] args) {
         int [] numbers = {3, 0, -7, 4, -1, 10, 65, -3, 35, 1};
         int sum = 0;
         int product = 1;
-    //ghghghghghghghgh
-        boolean positive = false;
-        for(int i = 0; i<5; i++){
 
-        }
+            int checkcount=0;
+            for(int i : numbers){
+                if(i>0){checkcount ++;}else break;
+            }
+            if(checkcount==5){
+                for(int i=0; i<5; i++){sum += numbers[i];}  System.out.println("The sum of first 5 numbers is: "+sum);
+            } else for(int i=5; i< numbers.length; i++){product *= numbers[i];}System.out.println("The product of first 5 numbers is: "+product);;
 
 
-//        for(int num : numbers){
-//            if (num > 0 ){
-//               for(int i=0; i<5; i++){
-//                   sum += num;
-//               }
-//
-//            } else {
-//                for(int i=5; i<10; i++){
-//                    product = product*num;
-//                }
-//
-//            }
-//        }
-//        System.out.println("The sum of the first 5 elements is " + sum );
-//        System.out.println("The product of the last 5 elements is " + product );
 
-    }
-
-}
+}}
