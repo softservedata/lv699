3a3aeba42d2a9ee060a1e686e5b70cbdcddebd50
VictoriaShevliuk <viktoriasvyatova37@gmail.com>
2022-10-26 16:57:35 +0300

    hw7 bug fixes

--- Viktoria_Shevliuk/Homework7/Homework7_OOP2/src/hw1/Employee.java
+++ Viktoria_Shevliuk/Homework7/Homework7_OOP2/src/hw1/Employee.java
@@ -1,15 +1,15 @@
 package hw1;
 
 public class Employee {
-    private String employeeId;
-    private String name;
+     String employeeId;
+     String name;
 
     public Employee(String employeeId, String name) {
         this.employeeId = employeeId;
         this.name = name;
     }
 
-    @Override
+
     public double calculatePay() {
         return 0;
     }
--- Viktoria_Shevliuk/Homework7/Homework7_OOP2/src/hw1/Run.java
+++ Viktoria_Shevliuk/Homework7/Homework7_OOP2/src/hw1/Run.java
@@ -1,4 +1,3 @@
-/*додати гетери та сетери до імплої, вирішити проблему з недоступним оверайдом обрахунку зп*/
 
 package hw1;
 
@@ -12,8 +11,8 @@ public class Run {
         List<Employee> emp = new ArrayList<>();
         emp.add(new SalariedEmployee("id000001", "Emp1", "qw101", 45.65, 120));
         emp.add(new SalariedEmployee("id000002", "Emp2", "qw202", 34.15, 100));
-        emp.add(new ContractEmployee("id000003", "Emp3", "qw303", 40000));
-        emp.add(new ContractEmployee("id000004", "Emp4", "qw404", 50000));
+        emp.add(new ContractEmployee("id000003", "Emp3", "qw303", 4050));
+        emp.add(new ContractEmployee("id000004", "Emp4", "qw404", 5701));
 
         for (Employee e : emp) {
             if (e instanceof ContractEmployee) {
@@ -29,9 +28,9 @@ public class Run {
                 return (int) -(o1.calculatePay() - o2.calculatePay());
             }
         });
-        //Arrange the entire sequence of workers descending the average monthly wage.
-        //Output the employee ID, name, and the average monthly wage for all elements of the list.
-        System.out.println("sequence of workers descending the average monthly wage: \n");
+
+
+        System.out.println("Sequence of workers descending the average monthly wage: \n");
         for (Employee each : emp) {
             System.out.println("employee ID: " + each.employeeId + ", employee name: " +
                     each.name + ", average monthly wage: " +  each.calculatePay());
